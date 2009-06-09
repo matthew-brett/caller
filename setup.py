da5e819fe9328f694c73264e00e863bf76db68f8
Matthew Brett <matthew.brett@gmail.com>
2009-06-08 18:01:24 -0700

    Initial code structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+''' Installation script for caller package '''
+
+from distutils.core import setup
+
+setup(name='caller',
+      version='0.1a',
+      description='Draft command line caller',
+      author='Matthew Brett',
+      author_email='matthew.brett@gmail.com',
+      url='None',
+      packages=['caller'],
+      )
+
